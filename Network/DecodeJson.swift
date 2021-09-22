2993e70040020f438a77572a8fed1e1555966c7a
Mike <mikael.galliot@appexperts.me.uk>
2021-09-22 14:54:28 +0100

    Added Network and WeatherForcast model

---
+++ Network/DecodeJson.swift
@@ -0,0 +1,28 @@
+//
+//  DecodeJson.swift
+//  Weather
+//
+//  Created by MAC on 25/08/21.
+//
+
+import Foundation
+
+protocol DecodeJson {
+    func decodeObject<T:Decodable>(input:Data, type:T.Type)-> T?
+    func decodeArray<T:Decodable>(input:Data, type:T.Type)-> [T]?
+}
+
+extension DecodeJson {
+    func decodeObject<T:Decodable>(input:Data, type:T.Type)-> T? {
+        
+        do {
+            try JSONDecoder().decode(T.self, from: input)
+        }catch {
+            print(error.localizedDescription)
+        }
+        return  try? JSONDecoder().decode(T.self, from: input)
+    }
+    func decodeArray<T:Decodable>(input:Data, type:T.Type)-> [T]? {
+        return  try? JSONDecoder().decode([T].self, from: input)
+    }
+}
---
+++ Network/EndPoint.swift
@@ -0,0 +1,23 @@
+//
+//  EndPoint.swift
+//  Weather
+//
+//  Created by MAC on 25/08/21.
+//
+
+import Foundation
+
+
+
+enum EndPoint {
+    static let baseUrl = "https://api.openweathermap.org/data/2.5"
+    static let appId = "fad7be6d3efab6853151833dfe5b49c2"
+    
+//    https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+}
+
+enum APIPath:String {
+    case weather = "/weather"
+    case onecall = "/onecall"
+}
